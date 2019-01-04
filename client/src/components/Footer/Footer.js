ac9a92dcf22a0ed347bcd2af43eb985938a27e33
Kyle Thomas <kthomas852@yahoo.com>
2019-01-03 18:46:29 -0700

    First commit

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <hr />
+    <p className="pull-right">
+      <i className="fa fa-github" aria-hidden="true" /> It's React.js and I helped!
+    </p>
+  </footer>
+);
+
+export default Footer;
